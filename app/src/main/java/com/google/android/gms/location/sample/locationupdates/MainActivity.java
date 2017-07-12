4945ccad0ed0423b3067206808f6aa7dbdf3fb8c
xueyuxy <yu.xue@mail.utoronto.ca>
2017-07-12 14:58:23 -0400

    UI is now workable

--- app/src/main/java/com/google/android/gms/location/sample/locationupdates/MainActivity.java
+++ app/src/main/java/com/google/android/gms/location/sample/locationupdates/MainActivity.java
@@ -177,7 +177,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
